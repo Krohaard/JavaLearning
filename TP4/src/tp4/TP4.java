16a5d956da747c8dd3f54a5e1cc27ed331f93f74
Krohaard <137874632+Krohaard@users.noreply.github.com>
2025-01-09 12:25:50 +0100

    Corrected TP4

--- TP4/src/tp4/TP4.java
+++ TP4/src/tp4/TP4.java
@@ -17,14 +17,14 @@ public class TP4 {
 		// TODO Auto-generated method stub
 		String kValue;
 		Scanner kRead = new Scanner(System.in);
-		System.out.println("Bonjour!! Écris moi quelque chose");
+		System.out.println("Bonjour!! Écris moi quelque chose :");
 		kValue = kRead.nextLine();
 		kRead.close();
 		if( kValue.length() > 0 ) {
 			System.out.println("Majuscules : " + kValue.toUpperCase());
 			System.out.println("Minuscules : " + kValue.toLowerCase());
 			System.out.println("Nombres de caractères : " + kCountChar(kValue));
-			System.out.println("Nombres de mots : " + (kValue.split(" ")).length);
+			System.out.println("Nombres de mots : " + kCountWords(kValue));
 		}else {
 			System.out.println("Entrée vide");
 		}
@@ -33,8 +33,14 @@ public class TP4 {
 		int kk;
 		int kCount=0;
 		for(kk=0;kk<kLine.length();kk++) {
-			if((kLine.charAt(kk)>='a' && kLine.charAt(kk)<='z') || ((kLine.charAt(kk)>='A' && kLine.charAt(kk)<='Z'))) kCount++;
+			if(Character.isLetter(kLine.charAt(kk))) kCount++;
 		}
 		return kCount;
 	}
+    public static int kCountWords(String kLine) {
+    	String[] kWords;
+		// Suppression des espaces multiples
+		kWords = kLine.trim().split("\\s+");
+		return kWords.length;
+    }
 }
