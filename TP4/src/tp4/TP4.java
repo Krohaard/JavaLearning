d798a51bf308a1ea6234c213d2c509d99ce00727
Krohaard <137874632+Krohaard@users.noreply.github.com>
2025-01-09 12:17:37 +0100

    TP4 finished

--- TP4/src/tp4/TP4.java
+++ TP4/src/tp4/TP4.java
@@ -3,6 +3,8 @@
  */
 package tp4;
 
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -13,7 +15,26 @@ public class TP4 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String kValue;
+		Scanner kRead = new Scanner(System.in);
+		System.out.println("Bonjour!! Écris moi quelque chose");
+		kValue = kRead.nextLine();
+		kRead.close();
+		if( kValue.length() > 0 ) {
+			System.out.println("Majuscules : " + kValue.toUpperCase());
+			System.out.println("Minuscules : " + kValue.toLowerCase());
+			System.out.println("Nombres de caractères : " + kCountChar(kValue));
+			System.out.println("Nombres de mots : " + (kValue.split(" ")).length);
+		}else {
+			System.out.println("Entrée vide");
+		}
 	}
-
+	public static int kCountChar(String kLine) {
+		int kk;
+		int kCount=0;
+		for(kk=0;kk<kLine.length();kk++) {
+			if((kLine.charAt(kk)>='a' && kLine.charAt(kk)<='z') || ((kLine.charAt(kk)>='A' && kLine.charAt(kk)<='Z'))) kCount++;
+		}
+		return kCount;
+	}
 }
