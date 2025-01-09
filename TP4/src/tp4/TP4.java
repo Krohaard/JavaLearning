fc800d190ccd1b4a93fc7097bc53eda44c9a37f8
Krohaard <137874632+Krohaard@users.noreply.github.com>
2025-01-09 11:52:48 +0100

    Creation of project TP4

---
+++ TP4/src/tp4/TP4.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package tp4;
+
+/**
+ * 
+ */
+public class TP4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
