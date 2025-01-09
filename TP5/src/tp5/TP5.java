295f65b9d0ee37a4e0a7d8c632f2b981818a4de0
Krohaard <137874632+Krohaard@users.noreply.github.com>
2025-01-10 00:25:11 +0100

    TP5 finished

--- TP5/src/tp5/TP5.java
+++ TP5/src/tp5/TP5.java
@@ -1,43 +1,48 @@
 package tp5;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TP5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(kSearchDivider(25));
-	}
-	public static int[] kSearchDivider(float kValue) {
-		int kDivider;
 		int kk;
-		int jj=0;
-		int[] kResultTemp;
-		int kIntTemp;
-		float kTemp;
-		kResultTemp = new int[jj];
-		kDivider=(int) (kValue/2);
-		for(kk=1;kk<=kDivider;kk++) {
-			kIntTemp = (int) (kValue / kk);
-			kTemp = (kValue/kk)-kIntTemp;
-			if(kTemp==0) {
-				kResultTemp = new int[jj+1];
-				kResultTemp[jj]=kk;
-				jj++;
+		Scanner kRead = new Scanner(System.in);
+		System.out.printf("Entrez un nombre positif :");
+		kk = kRead.nextInt();
+		if(kCheckPerfectNumber(kk) && kk > 0) {
+			System.out.printf("%d est un nombre parfait\n",kk);
+			System.out.println("Diviseurs propres :" + kSearchDivider(kk));
+		}else {
+			if(!kCheckPerfectNumber(kk)) {
+				System.out.printf("%d n'est pas un nombre parfait\n",kk);
+			}else {
+				System.out.println("Erreur de saisie\n");
+			}
+		}
+	}
+	public static ArrayList<Integer> kSearchDivider(float kValue) {
+		int kMaxDivider;
+		int kk;
+		ArrayList<Integer> kResultTemp = new ArrayList<>();
+		kMaxDivider = (int) (kValue / 2);
+		for(kk=1;kk<=kMaxDivider;kk++) {
+			if(((kValue/kk) - ((int) (kValue/kk)))==0) {
+				kResultTemp.add(kk);
 			}
 		}
 		return kResultTemp;
 	}
-	public static int[] kIncreaseArray(int[] kData) {
-		int[] kDataTemp;
-		int kSizeArray;
-		int kk;
-		kSizeArray = kData.length;
-		kDataTemp = new int[kSizeArray + 1];
-		for(kk=0;kk<kSizeArray) {
-			kDataTemp[kk]=kData[kk];
+	public static int kSumArray(ArrayList<Integer> kData) {
+		int kSum=0;
+		for(int kElement : kData) {
+			kSum = kSum + kElement;
 		}
-		return kDataTemp;
+		return kSum;
+	}
+	public static boolean kCheckPerfectNumber(int kValue) {
+		if(kSumArray(kSearchDivider(kValue))==kValue) return true;
+		return false;
 	}
 }
