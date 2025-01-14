06e4ac9464253b2e1973a1c3e955a1f2b27e5f14
Krohaard <137874632+Krohaard@users.noreply.github.com>
2025-01-15 00:39:07 +0100

    Creation of the TP9
    Signed-off-by: Krohaard <137874632+Krohaard@users.noreply.github.com>

---
+++ TP9/src/tp9/TP9.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package tp9;
+
+/**
+ * 
+ */
+public class TP9 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
