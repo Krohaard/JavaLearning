7673c14f08dea59b2146fd4f1c82d2835f87cdb3
Krohaard <137874632+Krohaard@users.noreply.github.com>
2025-01-19 00:12:13 +0100

    TP010 finished
    Signed-off-by: Krohaard <137874632+Krohaard@users.noreply.github.com>

--- TP010/src/tp010/TP010.java
+++ TP010/src/tp010/TP010.java
@@ -17,10 +17,13 @@ public class TP010 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayList<Integer> kRandList;
-		kRandList= kRandomList(10);
+		kRandList = kRandomList(10);
 		System.out.println("Liste originale : " + kRandList);
 		System.out.println("Plus grand : " + kMaxList(kRandList));
 		System.out.println("Plus petit : " + kMinList(kRandList));
+		System.out.println("Somme : " + kSumList(kRandList));
+		System.out.println("Moyenne : " + kMeanList(kRandList));
+		System.out.println("Liste après suppression des nombres pairs : " + kRemoverEven(kRandList));
 	}
 	public static ArrayList<Integer> kRandomList(int kNumber){
 		int kMin;
@@ -52,5 +55,22 @@ public class TP010 {
 		}
 		return kMax;
 	}
-
+	public static int kSumList(ArrayList<Integer> kData) {
+		int kSum=0;
+		for(Integer kElement : kData) {
+			kSum += kElement;
+		}
+		return kSum;
+	}
+	public static float kMeanList(ArrayList<Integer> kData) {
+		return (kSumList(kData)/kData.size());
+	}
+	public static ArrayList<Integer> kRemoverEven(ArrayList<Integer> kData){
+		ArrayList<Integer> kTemp=(ArrayList<Integer>) kData.clone();
+		for(Integer kElement : kData) {
+			if((kElement % 2) == 0)
+				kTemp.remove(kTemp.indexOf(kElement));
+		}
+		return kTemp;
+	}
 }
