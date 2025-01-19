cc3bfa762664be6d1d931ba474ebb6fb2cc1cd7a
Krohaard <137874632+Krohaard@users.noreply.github.com>
2025-01-19 11:02:07 +0100

    Exercice 10 corrected
    Signed-off-by: Krohaard <137874632+Krohaard@users.noreply.github.com>

--- TP010/src/tp010/TP010.java
+++ TP010/src/tp010/TP010.java
@@ -26,7 +26,7 @@ public class TP010 {
 			System.out.println("Plus petit : " + kMinList(kRandList));
 			System.out.println("Somme : " + kSumList(kRandList));
 			System.out.println("Moyenne : " + kMeanList(kRandList));
-			System.out.println("Liste après suppression des nombres pairs : " + kRemoverEven(kRandList.iterator()));
+			System.out.println("Liste après suppression des nombres pairs : " + kRemoverEven(kRandList));
 		} catch(Exception e) {
 			
 		}
@@ -38,6 +38,9 @@ public class TP010 {
 		ArrayList<Integer> kList;
 		Random kRandom;
 
+		if (kNumber <= 0) {
+		    throw new IllegalArgumentException("Le nombre d'éléments doit être positif.");
+		}
 		kMin =1;
 		kMax = 100;
 		kList = new ArrayList<>();
@@ -60,22 +63,22 @@ public class TP010 {
 		return Collections.max(kData);
 	}
 	public static int kSumList(ArrayList<Integer> kData) {
-		int kSum=0;
 		if (kData.isEmpty()) {
 		    throw new IllegalArgumentException("La liste est vide.");
 		}
-		for(Integer kElement : kData) {
-			kSum += kElement;
-		}
-		return kSum;
+		return kData.stream().mapToInt(Integer::intValue).sum();
 	}
 	public static float kMeanList(ArrayList<Integer> kData) {
+		if (kData.isEmpty()) {
+		    throw new IllegalArgumentException("La liste est vide.");
+		}
 		return (float) kSumList(kData)/kData.size();
 	}
-	public static Iterator<Integer> kRemoverEven(Iterator<Integer> kData){
-		while (kData.hasNext()) {
-		    if (kData.next() % 2 == 0) {
-		    	kData.remove();
+	public static ArrayList<Integer> kRemoverEven(ArrayList<Integer> kData){
+		Iterator<Integer> cloneData = kData.iterator();
+		while (cloneData.hasNext()) {
+		    if (cloneData.next() % 2 == 0) {
+		    	cloneData.remove();
 		    }
 		}
 		return kData;
