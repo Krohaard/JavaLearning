b219b566c24baa9e0ab6c89d8d766b14bd827edf
Krohaard <137874632+Krohaard@users.noreply.github.com>
2025-01-21 15:31:35 +0100

    update of tp011
    Signed-off-by: Krohaard <137874632+Krohaard@users.noreply.github.com>

---
+++ TP011/src/tp011/Students.java
@@ -0,0 +1,48 @@
+package tp011;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Students {
+	private HashMap<String, Float> kStudents;
+	private float kAverage;
+	private HashMap<String, Float> kFirstStudent;
+	private HashMap<String, Float> kLastStudent;
+
+	public Students() {
+		this.kStudents = new HashMap<>();
+		this.kFirstStudent = new HashMap<>();
+//		this.kFirstStudent.put("", (float) 0);
+		this.kLastStudent = new HashMap<>();
+//		this.kLastStudent.put("", (float) 0);
+		this.kAverage = 0;
+	}
+	private void CalculateAverage() {
+		this.kAverage = (float) this.kStudents.values().stream().mapToInt(Float::intValue).sum()/this.kStudents.size();
+	}
+	private void SetFirstStudent(String kName, float kNote) {
+		this.kFirstStudent.clear();
+		this.kFirstStudent.put(kName, kNote);
+	}
+	private void SetLastStudent(String kName, float kNote) {
+		this.kLastStudent.clear();
+		this.kLastStudent.put(kName, kNote);
+	}
+	public void AddNote(String kName, float kNote) {
+		this.kStudents.put(kName, kNote);
+		this.CalculateAverage();
+		if(Collections.max(this.kStudents.values()) <= kNote ) SetFirstStudent(kName,kNote);
+		if(Collections.min(this.kStudents.values()) >= kNote ) SetLastStudent(kName,kNote);
+	}
+	public float GetAverage() {
+		return kAverage;
+	}
+	public String FirstStudent() {
+		return kFirstStudent.values().toString().replaceAll("[\\[\\]]", "") + " ( " + (kFirstStudent.keySet().toString()).replaceAll("[\\[\\]]", "") + " )";
+	}
+	public String LastStudent() {
+		return kLastStudent.values().toString().replaceAll("[\\[\\]]", "") + " ( " + (kLastStudent.keySet().toString()).replaceAll("[\\[\\]]", "") + " )";
+	}
+}
--- TP011/src/tp011/TP011.java
+++ TP011/src/tp011/TP011.java
@@ -3,6 +3,10 @@
  */
 package tp011;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -13,7 +17,36 @@ public class TP011 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		float kNote;
+		String kName;
+		Scanner kRead;
+		Students kNotes;
+		
+		kNotes = new Students();
+
+		kRead = new Scanner(System.in);
+		try {
+			do {
+				kQuestion("Entrez le nom de l'étudiant : ");
+				kName = kRead.nextLine().trim();
+				if(kName.compareToIgnoreCase("fin")!=0) {
+					kQuestion("Entrez la note de " + kName + " : ");
+					kNote = kRead.nextFloat();
+					kRead.nextLine();
+					kNotes.AddNote(kName, kNote);
+				}
+			} while(kName.compareToIgnoreCase("fin")!=0);
+			System.out.printf("Moyenne de la classe : %.2f\n",kNotes.GetAverage());
+			System.out.printf("La note la plus haute : %s\n",kNotes.FirstStudent());
+			System.out.printf("La note la plus basse : %s\n",kNotes.LastStudent());
+		} catch (Exception e) {
+			
+		} finally {
+			kRead.close();
+		}
 
 	}
-
+	private static void kQuestion(String kSentence) {
+		System.out.printf(kSentence);
+	}
 }
