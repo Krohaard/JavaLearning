c9d8abef43055e1f8af440d7ac716fce5cac9dc1
Krohaard <137874632+Krohaard@users.noreply.github.com>
2025-02-17 13:03:28 +0100

    TP001 finished
    Signed-off-by: Krohaard <137874632+Krohaard@users.noreply.github.com>

---
+++ TP001/src/tp001/TP001.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package tp001;
+
+/**
+ * 
+ */
+public class TP001 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
