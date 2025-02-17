363d9696f99c8f79a7eb4b6e2aee67809446e9b5
Krohaard <137874632+Krohaard@users.noreply.github.com>
2025-02-17 13:12:43 +0100

    TP002 initialized
    Signed-off-by: Krohaard <137874632+Krohaard@users.noreply.github.com>

---
+++ TP002/src/tp002/TP002.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package tp002;
+
+/**
+ * 
+ */
+public class TP002 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
