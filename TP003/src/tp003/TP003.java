fea05156004e6b6d60a7cb572add30d3d85d1ef4
Krohaard <137874632+Krohaard@users.noreply.github.com>
2025-02-19 12:35:11 +0100

    creation of tp003
    Signed-off-by: Krohaard <137874632+Krohaard@users.noreply.github.com>

---
+++ TP003/src/tp003/TP003.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package tp003;
+
+/**
+ * 
+ */
+public class TP003 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
