a5594aac3d2d4e333fe2ee9713c7d5953e5899d1
Krohaard <137874632+Krohaard@users.noreply.github.com>
2025-02-19 14:34:01 +0100

    tp003 finished
    Signed-off-by: Krohaard <137874632+Krohaard@users.noreply.github.com>

--- TP003/src/tp003/TP003.java
+++ TP003/src/tp003/TP003.java
@@ -13,7 +13,14 @@ public class TP003 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int kNumber1;
+		int kNumber2;
+		kNumber1=50;
+		kNumber2=10;
+		System.out.printf("%d + %d = %d\n",kNumber1,kNumber2,kNumber1+kNumber2);
+		System.out.printf("%d - %d = %d\n",kNumber1,kNumber2,kNumber1-kNumber2);
+		System.out.printf("%d * %d = %d\n",kNumber1,kNumber2,kNumber1*kNumber2);
+		System.out.printf("%d / %d = %d\n",kNumber1,kNumber2,kNumber1/kNumber2);
 	}
 
 }
---
+++ TP003/src/tp003_2/TP003.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package tp003_2;
+
+/**
+ * 
+ */
+public class TP003 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int kNumber;
+		kNumber = 0;
+		if(kNumber > 0) {
+			System.out.println("le nombre est positif.");
+		} else if(kNumber < 0) {
+				System.out.println("le nombre est négatif.");
+		} else {
+			System.out.println("le nombre est égal à zéro");
+		}
+	}
+
+}
