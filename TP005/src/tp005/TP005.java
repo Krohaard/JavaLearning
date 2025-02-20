d67d6d730bdf814f714b3f232630d1463946b812
Krohaard <137874632+Krohaard@users.noreply.github.com>
2025-02-20 09:57:10 +0100

    Exercice 2 of TP005 is finished.
    Signed-off-by: Krohaard <137874632+Krohaard@users.noreply.github.com>

--- TP005/src/tp005/TP005.java
+++ TP005/src/tp005/TP005.java
@@ -3,6 +3,8 @@
  */
 package tp005;
 
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -13,7 +15,24 @@ public class TP005 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Person kPeople1;
+		Person kPeople2;
+		String kName;
+		int kAge;
+		Scanner kRead;
+		kRead = new Scanner(System.in);
+		System.out.printf("Quel est le nom de la première personne: ");
+		kName = kRead.nextLine();
+		System.out.printf("Quel est l'age de la première personne: ");
+		kAge = kRead.nextInt();kRead.nextLine();
+		kPeople1 = new Person(kName,kAge);
+		System.out.printf("Quel est le nom de la seconde personne: ");
+		kName = kRead.nextLine();
+		System.out.printf("Quel est l'age de la seconde personne: ");
+		kAge = kRead.nextInt();kRead.nextLine();
+		kPeople2 = new Person(kName,kAge);
+		kPeople1.displayDetails();
+		kPeople2.displayDetails();
+		kRead.close();
 	}
-
 }
