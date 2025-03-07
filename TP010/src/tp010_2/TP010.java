6c9d2f6e603a4f5f70473ffa32c23ddcd836e6a7
Krohaard <137874632+Krohaard@users.noreply.github.com>
2025-03-07 16:40:31 +0100

    creation of TP010
    Signed-off-by: Krohaard <137874632+Krohaard@users.noreply.github.com>

---
+++ TP010/src/tp010_2/TP010.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package tp010_2;
+
+/**
+ * 
+ */
+public class TP010 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
