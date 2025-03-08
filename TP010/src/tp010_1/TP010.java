61ede769069d69804e1f7bc2b33817cd42cf0918
Krohaard <137874632+Krohaard@users.noreply.github.com>
2025-03-09 00:06:10 +0100

    TP010 finished
    Signed-off-by: Krohaard <137874632+Krohaard@users.noreply.github.com>

--- TP010/src/tp010_1/TP010.java
+++ TP010/src/tp010_1/TP010.java
@@ -3,6 +3,12 @@
  */
 package tp010_1;
 
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
 /**
  * 
  */
@@ -13,7 +19,33 @@ public class TP010 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Random kRandom;
+		char kLetter;
+		int kk;
+		int ll;
+		String kText;
+		kText = new String();
+		kRandom = new Random();
+		for(ll=0;ll<15;ll++) {
+			for(kk=0;kk<49;kk++) {
+				kLetter = (char) (kRandom.nextInt(91+26+5-65)+65);
+				kText = kText + kLetter;
+			}
+			kText = kText + "\n";
+		}
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter("fichier.txt"))){
+			bw.write(kText);
+		} catch(IOException e) {
+			System.out.println(e);
+		}
 
+		try(FileReader reader = new FileReader("fichier.txt")){
+			int character;
+			while ((character = reader.read()) != -1) {
+				System.out.print((char) character);
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
-
 }
--- TP010/src/tp010_2/TP010.java
+++ TP010/src/tp010_2/TP010.java
@@ -3,6 +3,10 @@
  */
 package tp010_2;
 
+import java.util.Scanner;
+
+import krohaard.KFunc;
+
 /**
  * 
  */
@@ -13,7 +17,21 @@ public class TP010 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int kNumber1;
+		int kNumber2;
+		int kResult;
+		Scanner kInput;
+		kInput = new Scanner(System.in);
+		kNumber1 = (int) KFunc.kQuestion(kInput, "Entrez le premier nombre: ", "integer");
+		kNumber2 = (int) KFunc.kQuestion(kInput, "Entrez le second nombre: ", "integer");
+		try {
+			kResult = kNumber1/kNumber2; // Cela lèvera une ArithmeticException
+			System.out.println(kResult);
+		} catch (ArithmeticException e) {
+//			e.printStackTrace();
+			System.out.println(e.getMessage());
+//			System.out.println("Erreur : Division par zéro.");
+		}
 	}
 
 }
