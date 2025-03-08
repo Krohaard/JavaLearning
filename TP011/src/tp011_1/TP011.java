07d95fa19a2a6c92f0bf3b683fbdfc1ec1729999
Krohaard <137874632+Krohaard@users.noreply.github.com>
2025-03-09 00:12:16 +0100

    Creation of TP011
    Signed-off-by: Krohaard <137874632+Krohaard@users.noreply.github.com>

---
+++ TP011/src/tp011_1/TP011.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package tp011_1;
+
+/**
+ * 
+ */
+public class TP011 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
