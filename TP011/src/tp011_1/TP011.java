ddd8f184facc76c2fd6aea635db350676076b790
Krohaard <137874632+Krohaard@users.noreply.github.com>
2025-03-10 16:58:14 +0100

    TP011 finished (exercice 1 and exercice 2)
    krohaard

--- TP011/src/tp011_1/TP011.java
+++ TP011/src/tp011_1/TP011.java
@@ -3,6 +3,15 @@
  */
 package tp011_1;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Random;
+
 /**
  * 
  */
@@ -13,7 +22,79 @@ public class TP011 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Random kRandom;
+		char kLetter;
+		int kk;
+		int ll;
+		int kEnd;
+		int kLenght;
+		int kSpace;
+		String kText;
+		StringBuilder kFile;
+		
+		kText = new String();
+		kRandom = new Random();
+		kFile = new StringBuilder();
+		kLenght = kRandom.nextInt(101)+19;
+		kSpace = kRandom.nextInt(12)+6;
+		kLetter = 0;
+		for(ll=0;ll<kLenght;ll++) {
+			kEnd = kRandom.nextInt(101)+49;
+			for(kk=0;kk<kEnd;kk++) {
+				while((kLetter<65 || kLetter>90) && (kLetter>122 || kLetter<97)) {
+					kLetter = (char) (kRandom.nextInt(122-65)+65);
+				}
+				if(Math.ceilMod(kk, kSpace)==0) {
+					kText = kText + ' ';
+					kSpace = kRandom.nextInt(12)+6;
+					kk=kk+1;
+				}
+				kText = kText + kLetter;
+				kLetter = 0;
+			}
+			kText = kText + "\n";
+		}
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter("fichier.txt"))){
+			bw.write(kText);
+		} catch(IOException e) {
+			System.out.println(e);
+		}
+		//read the file txt
+		try {
+			kFile.append(new String(Files.readAllBytes(Paths.get("fichier.txt"))));
+		} catch (IOException e) {
+			System.out.println("Erreur de lecture du fichier : " + e.getMessage());
+		}
+		//copy the input file to the output file
+		try {
+			Files.write(Paths.get("fichierCopy.txt"), kFile.toString().getBytes());
+		} catch (IOException e) {
+			System.out.println("Erreur de lecture du fichier : " + e.getMessage());
+		}
+//other method
+		kFile.setLength(0);
+		kLenght = kRandom.nextInt(101)+19;
+		for(ll=0;ll<kLenght;ll++) {
+			kEnd = kRandom.nextInt(101)+49;
+			for(kk=0;kk<kEnd;kk++) {
+				kLetter = (char) (kRandom.nextInt(91+26+5-65)+65);
+				kFile.append(kLetter);
+			}
+			kFile.append("\n");
+		}
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter("fichier2.txt"))){
+			bw.write(kFile.toString());
+		} catch(IOException e) {
+			System.out.println(e);
+		}
+		kText = "";
+		try(BufferedReader kReader = new BufferedReader(new FileReader("fichier2.txt"));
+				BufferedWriter kWrite = new BufferedWriter(new FileWriter("Copyfichier2.txt"))){
+			while((kText = kReader.readLine()) != null) {
+				kWrite.write(kText);
+			}
+		} catch(IOException e) {
+			System.out.println(e);
+		}
 	}
-
 }
--- TP011/src/tp011_2/TP011.java
+++ TP011/src/tp011_2/TP011.java
@@ -3,6 +3,10 @@
  */
 package tp011_2;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /**
  * 
  */
@@ -13,7 +17,17 @@ public class TP011 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		StringBuilder kFile;
+		kFile = new StringBuilder();
+		String[] kList;
+		//read the file txt
+		try {
+			kFile.append(new String(Files.readAllBytes(Paths.get("fichier.txt"))));
+		} catch (IOException e) {
+			System.out.println("Erreur de lecture du fichier : " + e.getMessage());
+		}
+		kList = kFile.toString().split(" ");
+		System.out.printf("il y a %d mots dans ce texte.",kList.length);
 	}
 
 }
