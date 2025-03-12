6cf7601c3a7b6e191b9b6a65c4eef69e40bc4ac8
Krohaard <137874632+Krohaard@users.noreply.github.com>
2025-03-12 17:07:13 +0100

    Exercice 1 finished
    Krohaard
    The thread concept is little complex. I will try an other exercice on it
    later.

--- TP012/src/tp012_1/TP012.java
+++ TP012/src/tp012_1/TP012.java
@@ -3,17 +3,33 @@
  */
 package tp012_1;
 
+class OddEvent extends Thread{
+	public void run(){
+		for(int kk=0;kk<101;kk++) {
+			if ( (kk % 2 == 0) ) System.out.println("nombre impaire : " + kk);
+		}
+	}
+}
+class EvenEvent extends Thread{
+	public void run(){
+		for(int kk=0;kk<101;kk++) {
+			if ( (kk % 2 != 0) ) System.out.println("nombre paire : " + kk);
+		}
+	}
+}
 /**
  * 
  */
 public class TP012 {
-
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		OddEvent kOdd = new OddEvent();
+		EvenEvent kEven = new EvenEvent();
+		kOdd.start();
+		kEven.start();
 	}
 
 }
